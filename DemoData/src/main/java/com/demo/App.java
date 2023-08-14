46c330b6da8faf5b936f967294d58831b4e35b0e
J3-Aquacool <jagdish.java2@gmail.com>
2023-08-15 00:39:01 +0530

    hello im going

--- DemoData/src/main/java/com/demo/App.java
+++ DemoData/src/main/java/com/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! welcomemmm all agai");
+        System.out.println( "Hello World! welcome all");
     }
 }
